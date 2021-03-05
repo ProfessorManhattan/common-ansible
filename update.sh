0a9f391d9412481199ad9c7a10fef045033c0b0c
Brian Zalewski <brian@enviedsolutions.com>
2021-03-05 02:41:24 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,49 +4,63 @@
 # installed and then copies over base files from the modules. It also generates the
 # documentation.
 
-set -e
+set -ex
 
-if [ ! -d "./modules/docs" ]; then
-  cd modules
-  git submodule add -b master https://gitlab.com/megabyte-space/documentation/ansible.git docs
-  cd ..
+# Ensure submodules are up to date
+if [ ! -d "./.modules/docs" ]; then
+  git submodule add -b master https://gitlab.com/megabyte-space/documentation/ansible.git ./.modules/docs
 else
-  cd modules/docs
+  cd ./.modules/docs
   git checkout master && git pull origin master
   cd ../..
 fi
-if [ ! -d "./modules/shared" ]; then
-  cd modules
-  git submodule add -b master https://gitlab.com/megabyte-space/common/shared.git
-  cd ..
+if [ ! -d "./.modules/shared" ]; then
+  git submodule add -b master https://gitlab.com/megabyte-space/common/shared.git ./.modules/shared
 else
-  cd modules/shared
+  cd ./.modules/shared
   git checkout master && git pull origin master
   cd ../..
 fi
-cp -rf ./modules/shared/.github .
-cp -rf ./modules/shared/.gitlab .
-cp -rf ./modules/shared/.vscode .
-cp ./modules/shared/.cspell.json .cspell.json
-cp ./modules/shared/.editorconfig .editorconfig
-cp ./modules/shared/.flake8 .flake8
-cp ./modules/shared/.prettierrc .prettierrc
-cp ./modules/shared/.yamllint .yamllint
-cp ./modules/shared/CODE_OF_CONDUCT.md CODE_OF_CONDUCT.md
+
+# Copy over files from the shared submodule
+cp -Rf ./.modules/shared/.github .
+cp -Rf ./.modules/shared/.gitlab .
+cp -Rf ./.modules/shared/.vscode .
+cp ./.modules/shared/.cspell.json .cspell.json
+cp ./.modules/shared/.editorconfig .editorconfig
+cp ./.modules/shared/.flake8 .flake8
+cp ./.modules/shared/.yamllint .yamllint
+cp ./.modules/shared/CODE_OF_CONDUCT.md CODE_OF_CONDUCT.md
+
+# Replace the role_name placeholder with the repository folder name
 ROLE_FOLDER=$(basename "$PWD")
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  grep -rl 'MEGABYTE_ROLE_PLACEHOLDER' ./modules/ansible/files | xargs sed -i .bak "s/MEGABYTE_ROLE_PLACEHOLDER/${ROLE_FOLDER}/g"
-  find ./modules/ansible/files -name "*.bak" -type f -delete
+  grep -rl 'MEGABYTE_ROLE_PLACEHOLDER' ./.modules/ansible/files | xargs sed -i .bak "s/MEGABYTE_ROLE_PLACEHOLDER/${ROLE_FOLDER}/g"
+  find ./.modules/ansible/files -name "*.bak" -type f -delete
 else
-  grep -rl 'MEGABYTE_ROLE_PLACEHOLDER' ./modules/ansible/files | xargs sed -i "s/MEGABYTE_ROLE_PLACEHOLDER/${ROLE_FOLDER}/g"
+  grep -rl 'MEGABYTE_ROLE_PLACEHOLDER' ./.modules/ansible/files | xargs sed -i "s/MEGABYTE_ROLE_PLACEHOLDER/${ROLE_FOLDER}/g"
 fi
-cp -Rf ./modules/ansible/files/ .
-chmod 755 .husky/pre-commit
-cd ./modules/ansible
+
+# Copy files over from the Ansible shared submodule
+cp -Rf ./.modules/ansible/files/ .
+
+# Reset the Ansible shared module to HEAD
+cd ./.modules/ansible
 git reset --hard HEAD
 cd ../..
-jq -s '.[0] * .[1]' blueprint.json ./modules/docs/common.json > __bp.json | true
-npx -y @appnest/readme generate --config __bp.json --input ./modules/docs/blueprint-contributing.md --output CONTRIBUTING.md | true
-npx -y @appnest/readme generate --config __bp.json --input ./modules/docs/blueprint-readme.md | true
+
+# Ensure the pre-commit hook is executable
+chmod 755 .husky/pre-commit
+
+# Generate the documentation
+README_TEMPLATE=readme
+if [ -f ./main.yml ]; then
+  # main.yml is in directory so this must be the root playbook directory
+  README_TEMPLATE=readme-playbooks
+fi
+jq -s '.[0] * .[1]' blueprint.json ./.modules/docs/common.json > __bp.json | true
+npx -y @appnest/readme generate --config __bp.json --input ./.modules/docs/blueprint-contributing.md --output CONTRIBUTING.md | true
+npx -y @appnest/readme generate --config __bp.json --input ./.modules/docs/blueprint-$README_TEMPLATE.md | true
 rm __bp.json
-echo "Done updating meta files and generating documentation"
+
+echo "*** Done updating meta files and generating documentation ***"
