aff4612833534c100e804f943ffc19b73f676b50
Brian Zalewski <brian@megabyte.space>
2021-05-11 23:41:45 +0000

    Update .start.sh

--- .start.sh
+++ .start.sh
@@ -14,6 +14,7 @@ if [ ! -f "./.modules/${REPO_TYPE}/update.sh" ]; then
   git submodule add -b master https://gitlab.com/megabyte-space/common/$REPO_TYPE.git ./.modules/$REPO_TYPE
 else
   cd ./.modules/$REPO_TYPE
+  git config pull.rebase true
   git checkout master && git pull origin master
   cd ../..
 fi
