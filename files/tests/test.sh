2159bb28b14e4f22aacfd1ea08c7f98e86401dc5
Brian Zalewski <brian@megabyte.space>
2021-06-19 08:50:57 +0000

    Update files/tests/test.sh

--- files/tests/test.sh
+++ files/tests/test.sh
@@ -4,6 +4,5 @@ pip3 install ansible
 if [ -f requirements.yml ]; then ansible-galaxy install -r requirements.yml; fi
 cp tests/ansible.cfg ansible.cfg
 cp tests/test.yml main.yml
-ROLE_FOLDER=$(basename $PWD)
-sed -i '.bak' "s^{{PROJECT_FOLDER_NAME}}^$ROLE_FOLDER^g" main.yml
+sed -i '.bak' "s^{{PROJECT_FOLDER_NAME}}^$(basename "$PWD")^g" main.yml
 ansible-playbook -i tests/inventory main.yml
