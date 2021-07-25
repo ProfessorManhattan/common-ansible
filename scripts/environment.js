b72b1797cf89191435f06a216972808fb67e10cd
Megabyte Labs Automation <help@megabyte.space>
2021-07-25 08:19:09 +0000

    automation(upstream-sync) Applying changes from upstream repository.

--- scripts/environment.js
+++ scripts/environment.js
@@ -1,15 +1,15 @@
-'use strict'
+'use strict';
 
 /* eslint-disable space-before-function-paren */
 
-import inquirer from 'inquirer'
-import signale from 'signale'
-import * as fs from 'fs'
+import inquirer from 'inquirer';
+import signale from 'signale';
+import * as fs from 'fs';
 
 signale.info(
   'Answer the prompt below to switch between environments. Each environment should be a folder with folders and files you wish to link to from the root of the project. They should normally consist of a host_vars, group_vars, inventory, and files folder (but can contain any files/folders you wish to link). Each environment should have its own folder in the `environments/` folder titled as the name of the environment. After you select an answer, the script will symlink all of the items in the environments folder to the root as long as there is not anything except a symlink in the target location (i.e. it will overwrite symlinks but not files).'
-)
-promptForEnv()
+);
+promptForEnv();
 
 /**
  * Prompts the user for the environment they wish to use by asking them
@@ -24,16 +24,16 @@ async function promptForEnv() {
       message: 'Which environment would you like to use?',
       choices: getDirectories('environments/')
     }
-  ])
-  const env = response.environment
-  const elements = fs.readdirSync('environments/' + env + '/')
-  let flawless = true
+  ]);
+  const env = response.environment;
+  const elements = fs.readdirSync('environments/' + env + '/');
+  let flawless = true;
   for (const element of elements) {
-    const target = './environments/' + env + '/' + element
+    const target = './environments/' + env + '/' + element;
     if (fs.existsSync(element)) {
       fs.lstat(element, (err, stats) => {
         if (err) {
-          return signale.error('The logger encountered a fatal error!', err)
+          return signale.error('The logger encountered a fatal error!', err);
         }
         if (!stats.isSymbolicLink()) {
           signale.error(
@@ -44,31 +44,31 @@ async function promptForEnv() {
               '/' +
               element +
               '` because there is a non-symbolic link with the same name in the root of the project.'
-          )
-          flawless = false
+          );
+          flawless = false;
         } else {
-          fs.unlinkSync(element)
+          fs.unlinkSync(element);
           fs.symlinkSync(target, element, (err, stats) => {
             if (err) {
-              return signale.error('The logger encountered a fatal error!', err)
+              return signale.error('The logger encountered a fatal error!', err);
             }
-            signale.note(element + ' is now linked to environments/' + env + '/' + element + '.')
-          })
+            signale.note(element + ' is now linked to environments/' + env + '/' + element + '.');
+          });
         }
-      })
+      });
     } else {
       fs.symlinkSync(target, element, (err, stats) => {
         if (err) {
-          return signale.error('The logger encountered a fatal error!', err)
+          return signale.error('The logger encountered a fatal error!', err);
         }
-        signale.note(element + '/ is now linked to environments/' + env + '/' + element + '.')
-      })
+        signale.note(element + '/ is now linked to environments/' + env + '/' + element + '.');
+      });
     }
   }
   if (flawless) {
-    signale.success('The ' + env + ' environment is now active.')
+    signale.success('The ' + env + ' environment is now active.');
   } else {
-    signale.warn('There was an error linking the ' + env + ' environment.')
+    signale.warn('There was an error linking the ' + env + ' environment.');
   }
 }
 
@@ -80,6 +80,6 @@ async function promptForEnv() {
  */
 function getDirectories(path) {
   return fs.readdirSync(path).filter(function (file) {
-    return fs.statSync(path + '/' + file).isDirectory()
-  })
+    return fs.statSync(path + '/' + file).isDirectory();
+  });
 }
