d171bf211d05653443b8254c2c460a43fb0bd5f9
Megabyte Labs Automation <help@megabyte.space>
2021-08-05 06:39:01 +0000

    automation(upstream-sync) Applying changes from upstream repository.

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC1091
 
 # @file .common/scripts/init.sh
-# @brief Imports log functions and installs [task](https://github.com/go-task/task) if it is not installed
+# @brief Imports log functions and installs [Task](https://github.com/go-task/task) if it is not installed
 
 . "./.common/scripts/log.sh"
 
