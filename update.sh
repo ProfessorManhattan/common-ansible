d31bb3f935b7df45799e3656f93cb6f60847c30f
Brian Zalewski <brian@megabyte.space>
2021-08-07 09:43:41 +0000

    Update update.sh, files-role/Taskfile.yml, files-playbook/Taskfile.yml files Deleted lib.sh

--- update.sh
+++ update.sh
@@ -4,18 +4,17 @@
 # @file .common/update.sh
 # @brief Ensures the project is up-to-date with the latest upstream changes
 # @description
-#   This script performs maintenance on this repository. It includes several bash script
-#   libraries and then it:
+# This script performs maintenance on this repository. It includes several bash script
+# libraries and then it:
 #
-#   1. Ensures Node, jq, Task, and yq are installed
-#   2. Bootstraps the project by using Task to run initialization tasks which bootstrap the project
-#   3. Notifies the user about missing software dependencies that require root priviledges to install
+# 1. Ensures Node, jq, Task, and yq are installed
+# 2. Bootstraps the project by using Task to run initialization tasks which bootstrap the project
+# 3. Notifies the user about missing software dependencies that require root priviledges to install
 
 source "./.common/scripts/common.sh"
 source "./.common/scripts/log.sh"
 source "./.common/scripts/software.sh"
 source "./.common/scripts/notices.sh"
-source "./.common/lib.sh"
 
 if [ "${container:=}" != 'docker' ]; then
   info "Ensuring Node.js, Task, jq, and yq are installed"
@@ -27,11 +26,11 @@ if [ "${container:=}" != 'docker' ]; then
   success "Node.js, Task, jq, and yq are all installed"
 fi
 
-REPO_SUBTYPE=$((grep REPOSITORY_SUBTYPE Taskfile.yml) | sed 's/.*REPOSITORY_SUBTYPE: \(.*\)/\1/')
+REPO_SUBTYPE="$(grep REPOSITORY_SUBTYPE Taskfile.yml | sed 's/.*REPOSITORY_SUBTYPE: \(.*\)/\1/')"
 export REPO_SUBTYPE
 
 cp ".common/files-$REPO_SUBTYPE/Taskfile.yml" Taskfile.yml
-task common:update
+task start
 
 if [ "${container:=}" != 'docker' ]; then
   missingDockerNotice
