bc3282a80a2d55af0640da1d9f46d4bd314abe20
Megabyte Labs Automation <help@megabyte.space>
2021-11-20 20:59:25 +0000

    automation(upstream-sync) Applying changes from upstream repository.

--- .common/.gitlab/ci/scripts/update-init.sh
+++ .common/.gitlab/ci/scripts/update-init.sh
@@ -7,3 +7,13 @@ set -eo pipefail
 
 echo "Update script running.."
 npm install --save-dev @mblabs/eslint-config@latest
+TMP="$(mktemp)"
+jq 'del(."standard-version")' package.json > "$TMP"
+mv "$TMP" package.json
+TMP="$(mktemp)"
+jq 'del(."lint-staged")' package.json > "$TMP"
+mv "$TMP" package.json
+mkdir -p docs
+mv CODE_OF_CONDUCT.md docs || true
+mv CONTRIBUTING.md docs || true
+npm install --save-optional chalk inquirer signale
