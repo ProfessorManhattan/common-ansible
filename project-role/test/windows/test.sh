31f41d76ee5179fe2ae4347581945cce02aa913b
Brian Zalewski <brian@megabyte.space>
2021-11-27 03:47:40 +0000

    Update project-role/test/darwin/test.sh, project-role/test/windows/test.sh, project-role/test/linux/test.sh

--- project-role/test/windows/test.sh
+++ project-role/test/windows/test.sh
@@ -7,19 +7,36 @@
 #   This script is intended to be run in a WSL environment on a Windows host to provision the Windows
 #   host via Ansible using WinRM and CredSSP.
 
-# Ensure Ansible is installed
+TEST_TYPE='windows'
+
+# @description Ensure Ansible is installed along with required dependencies
 pip3 install ansible pywinrm[credssp]
 
-# Ensure Ansible Galaxy dependencies are installed
+# @description Ensure Ansible Galaxy dependencies are installed
 if [ -f requirements.yml ]; then
   ansible-galaxy install -r requirements.yml
 fi
 
-# Symlink the Ansible Galaxy role name to the working directory one level up
+# @description Symlink the Ansible Galaxy role name to the working directory one level up
 ROLE_NAME="$(grep "role:" test/windows/test.yml | sed 's^- role: ^^' | xargs)"
 ln -s "$(basename "$PWD")" "../$ROLE_NAME"
 
-# Copy required files and run the Ansible play
-cp test/windows/ansible.cfg ansible.cfg
-cp test/windows/test.yml test.yml
-ansible-playbook -i test/windows/inventory test.yml
+# @description Back up files and then copy replacements (i.e. ansible.cfg)
+function backupAndCopyFiles() {
+  if [ -f ansible.cfg ]; then
+    cp ansible.cfg ansible.cfg.bak
+  fi
+  cp "test/$TEST_TYPE/ansible.cfg" ansible.cfg
+}
+
+# @description Restore files that were backed up (i.e. ansible.cfg)
+function restoreFiles() {
+  if [ -f ansible.cfg.bak ]; then
+    mv ansible.cfg.bak ansible.cfg
+  fi
+}
+
+# @description Back up files, run the play, and then restore files
+backupAndCopyFiles
+ansible-playbook -i "test/$TEST_TYPE/inventory" "test/$TEST_TYPE/test.yml" || { restoreFiles; exit 1 }
+restoreFiles
