dab33c6435fd7ba935d9a5ef70ef5e3fd9560f33
Megabyte Labs Automation <help@megabyte.space>
2021-12-13 16:44:47 +0000

    ⤵️ automation(synchronize) Applying changes from upstream repository.

--- .gitlab/ci/scripts/update-init.sh
+++ .gitlab/ci/scripts/update-init.sh
@@ -37,6 +37,8 @@ else
   cp -rT common-shared/common/.gitlab/ci .gitlab/ci
 fi
 
+cp common-shared/common/.config/log .config/log
+
 # @description Ensure proper NPM dependencies are installed
 echo "Installing NPM packages"
 if [ ! -f 'package.json' ]; then
