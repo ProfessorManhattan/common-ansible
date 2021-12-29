b17781f0a6c20d5b274923597962c9122c59d5e3
Megabyte Labs Automation <help@megabyte.space>
2021-12-29 11:25:32 +0000

    ⤵️ automation(synchronize) Applying changes from upstream repository.

--- _generated_/common/.config/scripts/prompts/molecule/playbook.js
+++ _generated_/common/.config/scripts/prompts/molecule/playbook.js
@@ -1,7 +1,7 @@
 import chalk from 'chalk'
 import inquirer from 'inquirer'
-import * as fs from 'node:fs'
 import { execSync } from 'node:child_process'
+import * as fs from 'node:fs'
 import { decorateFiles } from '../lib/decorate-files.js'
 import { logInstructions, logRaw, LOG_DECORATOR_REGEX } from '../lib/log.js'
 
@@ -35,9 +35,8 @@ async function promptForInventory() {
     }
   ])
   const DECORATION_LENGTH = 2
-  const inventory = response.environment.replace(LOG_DECORATOR_REGEX, '').slice(DECORATION_LENGTH)
 
-  return inventory
+  return response.environment.replace(LOG_DECORATOR_REGEX, '').slice(DECORATION_LENGTH)
 }
 
 /**
@@ -45,25 +44,28 @@ async function promptForInventory() {
  *
  * @returns {Promise} Promise that resolves to execSync calls that run the main.yml playbook
  */
-// eslint-disable-next-line max-statements, require-jsdoc, max-lines-per-function
+// eslint-disable-next-line require-jsdoc
 async function run() {
   logInstructions('Run the Playbook', 'These set of prompts will run the main.yml playbook after you specify:\n\n')
   logRaw(chalk.bold('1. The "environment"'))
-  logRaw('\nThe environment is a collection of folders that should, at the very minimum, include "files",' +
-   ' "group_vars", "host_vars", and "inventories". Each folder in the "environments" folder constitutes a' +
-   ' different environment. By using environments, you can seperate different sets of variables/files or even' +
-   ' seperate your private variables out into a sub-module.\n')
+  logRaw(
+    '\nThe environment is a collection of folders that should, at the very minimum, include "files",' +
+      ' "group_vars", "host_vars", and "inventories". Each folder in the "environments" folder constitutes a' +
+      ' different environment. By using environments, you can seperate different sets of variables/files or even' +
+      ' seperate your private variables out into a sub-module.\n'
+  )
   logRaw(chalk.bold('2. An inventory file'))
   logRaw(
     '\nThe Ansible inventory stored in the "inventories" folder. This will generally be a YML file with host' +
-    ' connection information that also correlates the inventory with the proper host_vars and group_vars.' +
-    ' It is assumed that your sudo username and password are encrypted inside the inventory (via "ansible-vault").'
+      ' connection information that also correlates the inventory with the proper host_vars and group_vars.' +
+      ' It is assumed that your sudo username and password are encrypted inside the inventory (via "ansible-vault").'
   )
 
   // eslint-disable-next-line functional/no-try-statement
   try {
     execSync(`task environment`, { stdio: 'inherit' })
     const inventory = await promptForInventory()
+
     return execSync(`task playbook -- ${inventory}`, { stdio: 'inherit' })
   } catch {
     // eslint-disable-next-line no-process-exit
