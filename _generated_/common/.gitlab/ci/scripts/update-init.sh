9f4bb5c9fe34af07f8a1f195ed44d38c0172992d
Megabyte Labs <help@megabyte.space>
2022-02-03 08:46:06 +0000

    ⤵️ automation(synchronize) Applying changes from upstream repository.

--- _generated_/common/.gitlab/ci/scripts/update-init.sh
+++ _generated_/common/.gitlab/ci/scripts/update-init.sh
@@ -179,13 +179,6 @@ if test -f "ARCHITECTURE.md"; then
   mv ARCHITECTURE.md docs
 fi
 
-if type pnpm &> /dev/null && type jq &> /dev/null && [ "$(jq -r '.devDependencies.glob' package.json)" == 'null' ]; then
-  pnpm i --save-dev glob
-else
-  echo "OUTSIDE"
-  pnpm i --save-dev glob
-fi
-
 # @description Commit and push the changes
 if [ -n "$GITLAB_CI" ]; then
   git add --all
