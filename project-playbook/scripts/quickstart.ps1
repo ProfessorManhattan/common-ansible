8cae231a4525adeba356830d70ea842615b0c813
Brian Zalewski <brian@megabyte.space>
2022-07-12 04:49:25 +0000

    Update project-playbook/scripts/quickstart.ps1

--- project-playbook/scripts/quickstart.ps1
+++ project-playbook/scripts/quickstart.ps1
@@ -14,18 +14,36 @@
 
 New-Item -ItemType Directory -Force -Path C:\Temp
 
+# @description Reboot and continue script after reboot
+function RebootAndContinueIfRequired {
+  Install-Module -Name PendingReboot -Force
+  if ((Test-PendingReboot).IsRebootPending) {
+    Write-Host "Changing $env:UserName password to 'MegabyteLabs' so we can automatically log back in" -ForegroundColor Black -BackgroundColor Cyan
+    $NewPassword = ConvertTo-SecureString "MegabyteLabs" -AsPlainText -Force
+    Set-LocalUser -Name $env:UserName -Password $NewPassword
+    Write-Host "Turning on auto-logon and making script start C:\Temp\quickstart.ps1" -ForegroundColor Black -BackgroundColor Cyan
+    Set-AutoLogon -DefaultUsername "$env:UserName" -DefaultPassword "MegabyteLabs" -Script "c:\Temp\quickstart.ps1"
+    if (!(Test-Path "C:\Temp\quickstart.ps1")) {
+      Write-Host "Ensuring the recursive update script is downloaded"
+      Start-BitsTransfer -Source "https://install.doctor/windows-quickstart" -Destination "C:\Temp\quickstart.ps1" -Description "Downloading quickstart.ps1 script"
+    }
+    Restart-Computer
+  }
+}
+
 # @description Ensure all Windows updates have been applied and then starts the provisioning process
 function EnsureWindowsUpdated {
+    Write-Host "Ensuring all the available Windows updates have been applied." -ForegroundColor Black -BackgroundColor Cyan
     Get-WUInstall -AcceptAll -IgnoreReboot
-    if ((Test-PendingReboot).IsRebootPending) {
-        Restart-Computer -Wait
-    }
+    Write-Host "Checking if reboot is required." -ForegroundColor Black -BackgroundColor Cyan
+    RebootAndContinueIfRequired
 }
 
 # @description Ensures Microsoft-Windows-Subsystem-Linux feature is available
 function EnsureLinuxSubsystemEnabled {
     $wslenabled = Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux | Select-Object -Property State
     if ($wslenabled.State -eq "Disabled") {
+        Write-Host "Enabling Microsoft-Windows-Subsystem-Linux" -ForegroundColor Black -BackgroundColor Cyan
         Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
     }
 }
@@ -34,6 +52,7 @@ function EnsureLinuxSubsystemEnabled {
 function EnsureVirtualMachinePlatformEnabled {
     $vmenabled = Get-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform | Select-Object -Property State
     if ($vmenabled.State -eq "Disabled") {
+        Write-Host "Enabling VirtualMachinePlatform" -ForegroundColor Black -BackgroundColor Cyan
         Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
     }
 }
@@ -41,77 +60,76 @@ function EnsureVirtualMachinePlatformEnabled {
 # @description Ensures Ubuntu 20.04 is installed on the system from a .appx file
 function EnsureUbuntuAPPXInstalled {
     if(!(Test-Path "C:\Temp\UBUNTU2004.appx")) {
+        Write-Host "Downloading Ubuntu APPX" -ForegroundColor Black -BackgroundColor Cyan
         Start-BitsTransfer -Source "https://aka.ms/wslubuntu2004" -Destination "C:\Temp\UBUNTU2004.appx" -Description "Downloading Ubuntu 20.04 WSL image"
     }
     $ubu2004appxinstalled = Get-AppxPackage -Name CanonicalGroupLimited.Ubuntu20.04onWindows
     if (!$ubu2004appxinstalled) {
+        Write-Host "Adding Ubuntu APPX" -ForegroundColor Black -BackgroundColor Cyan
         Add-AppxPackage -Path "C:\Temp\UBUNTU2004.appx"
     }
 }
 
 # @description Automates the process of setting up the Ubuntu 20.04 WSL environment
 function SetupUbuntuWSL {
+    Write-Host "Setting up Ubuntu WSL" -ForegroundColor Black -BackgroundColor Cyan
     Start-Process "ubuntu.exe" -ArgumentList "install --root" -Wait -NoNewWindow
     $username = $env:username
+    Write-Host "Adding a user" -ForegroundColor Black -BackgroundColor Cyan
     Start-Process "ubuntu.exe" -ArgumentList "run adduser $username --gecos 'First,Last,RoomNumber,WorkPhone,HomePhone' --disabled-password" -Wait -NoNewWindow
+    Write-Host "Adding user to sudo group" -ForegroundColor Black -BackgroundColor Cyan
     Start-Process "ubuntu.exe" -ArgumentList "run usermod -aG sudo $username" -Wait -NoNewWindow
+    Write-Host "Enabling passwordless sudo privileges" -ForegroundColor Black -BackgroundColor Cyan
     Start-Process "ubuntu.exe" -ArgumentList "run echo '$username ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers" -Wait -NoNewWindow
+    Write-Host "Setting default user" -ForegroundColor Black -BackgroundColor Cyan
     Start-Process "ubuntu.exe" -ArgumentList "config --default-user $username" -Wait -NoNewWindow
 }
 
 # @description Ensures Docker Desktop is installed (which requires a reboot)
 function EnsureDockerDesktopInstalled {
     if (!(Test-Path "C:\Temp\docker-desktop-installer.exe")) {
+        Write-Host "Downloading Docker Desktop for Windows" -ForegroundColor Black -BackgroundColor Cyan
         Start-BitsTransfer -Source "https://download.docker.com/win/stable/Docker%20Desktop%20Installer.exe" -Destination "C:\Temp\docker-desktop-installer.exe" -Description "Downloading Docker Desktop"
     }
+    Write-Host "Installing Docker Desktop for Windows" -ForegroundColor Black -BackgroundColor Cyan
     Start-Process 'C:\Temp\docker-desktop-installer.exe' -ArgumentList 'install --quiet' -Wait -NoNewWindow
     & 'C:\Program Files\Docker\Docker\Docker Desktop.exe'
+    Write-Host "Waiting for Docker Desktop to come online" -ForegroundColor Black -BackgroundColor Cyan
     Start-Sleep -s 30
 }
 
 # @description Enables WinRM connectivity
 function EnableWinRM {
+    Write-Host "Enabling WinRM.." -ForegroundColor Black -BackgroundColor Cyan
     $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
     $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
     (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
-    powershell.exe -ExecutionPolicy ByPass -File $file -Verbose -EnableCredSSP -DisableBasicAuth -SkipNetworkProfileCheck
+    powershell.exe -ExecutionPolicy ByPass -File $file -Verbose -EnableCredSSP -DisableBasicAuth -ForceNewSSLCert -SkipNetworkProfileCheck
 }
 
 # @description Run the playbook
 function RunPlaybook {
+    Write-Host "Running quickstart.sh in WSL environment" -ForegroundColor Black -BackgroundColor Cyan
     Start-Process "ubuntu.exe" -ArgumentList "run curl -sSL https://gitlab.com/megabyte-labs/gas-station/-/raw/master/scripts/quickstart.sh > quickstart.sh && bash quickstart.sh" -Wait -NoNewWindow
+    Write-Host "Running quickstart continue command in WSL environment" -ForegroundColor Black -BackgroundColor Cyan
     Start-Process "ubuntu.exe" -ArgumentList "run bash ~/Playbooks/.cache/ansible-playbook-continue-command.sh" -Wait -NoNewWindow
 }
 
 # @description The main logic for the script - enable Windows features, set up Ubuntu WSL, and install Docker Desktop
 # while continuing script after a restart.
-workflow ProvisionWindowsWSLAnsible {
+function ProvisionWindowsWSLAnsible {
     Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
     Install-Module -Name PSWindowsUpdate -Force
-    Install-Module -Name PendingReboot -Force
-    EnsureWindowsUpdated
-    # Because of the error "A workflow cannot use recursion," we can just run the update process a few times to ensure everything is updated
-    EnsureWindowsUpdated
     EnsureWindowsUpdated
     EnableWinRM
     EnsureLinuxSubsystemEnabled
     EnsureVirtualMachinePlatformEnabled
-    if ((Test-PendingReboot).IsRebootPending) {
-        Restart-Computer -Wait
-    }
     EnsureUbuntuAPPXInstalled
     SetupUbuntuWSL
     EnsureDockerDesktopInstalled
-    Restart-Computer -Wait
+    RebootAndContinueIfRequired
     RunPlaybook
+    Write-Host "All done! Make sure you change your password. It was set to 'MegabyteLabs'" -ForegroundColor Black -BackgroundColor Cyan
 }
 
-
-$options = New-ScheduledJobOption -RunElevated -ContinueIfGoingOnBattery -StartIfOnBattery
-$secpasswd = ConvertTo-SecureString "Aa123456!" -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential ("WELCOME\Administrator", $secpasswd)
-$AtStartup = New-JobTrigger -AtStartup
-
-# Source: https://stackoverflow.com/questions/15166839/powershell-reboot-and-continue-script
-Register-ScheduledJob -Name ProvisionWindowsWSLAnsibleJob -Trigger $AtStartup -ScriptBlock ({[System.Management.Automation.Remoting.PSSessionConfigurationData]::IsServerManager = $true; Import-Module PSWorkflow; Resume-Job -Name RebootContinueWorkflow -Wait}) -ScheduledJobOption $options
-ProvisionWindowsWSLAnsible -AsJob -JobName RebootContinueWorkflow
+ProvisionWindowsWSLAnsible
