e4a9c2767a084eb93c1dbfb47ba11af7de010254
Brian Zalewski <brian@megabyte.space>
2022-08-22 03:30:54 +0000

    Update project-playbook/scripts/quickstart.ps1

--- project-playbook/scripts/quickstart.ps1
+++ project-playbook/scripts/quickstart.ps1
@@ -54,84 +54,84 @@ function RebootAndContinueIfRequired {
 
 # @description Ensure all Windows updates have been applied and then starts the provisioning process
 function EnsureWindowsUpdated {
-    if (!(Get-Module "PSWindowsUpdate")) {
-      Write-Host "Installing update module" -ForegroundColor Black -BackgroundColor Cyan
-      Install-Module -Name PSWindowsUpdate -Force
-    }
-    Write-Host "Ensuring all the available Windows updates have been applied." -ForegroundColor Black -BackgroundColor Cyan
-    Import-Module PSWindowsUpdate -Force
-    Get-WUInstall -AcceptAll -IgnoreReboot
-    Write-Host "Checking if reboot is required." -ForegroundColor Black -BackgroundColor Cyan
-    RebootAndContinueIfRequired
+  if (!(Get-Module "PSWindowsUpdate")) {
+    Write-Host "Installing update module" -ForegroundColor Black -BackgroundColor Cyan
+    Install-Module -Name PSWindowsUpdate -Force
+  }
+  Write-Host "Ensuring all the available Windows updates have been applied." -ForegroundColor Black -BackgroundColor Cyan
+  Import-Module PSWindowsUpdate -Force
+  Get-WUInstall -AcceptAll -IgnoreReboot
+  Write-Host "Checking if reboot is required." -ForegroundColor Black -BackgroundColor Cyan
+  RebootAndContinueIfRequired
 }
 
 # @description Ensures Microsoft-Windows-Subsystem-Linux feature is available
 function EnsureLinuxSubsystemEnabled {
-    $wslenabled = Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux | Select-Object -Property State
-    if ($wslenabled.State -eq "Disabled") {
-        Write-Host "Enabling Microsoft-Windows-Subsystem-Linux" -ForegroundColor Black -BackgroundColor Cyan
-        Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
-    }
+  $wslenabled = Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux | Select-Object -Property State
+  if ($wslenabled.State -eq "Disabled") {
+    Write-Host "Enabling Microsoft-Windows-Subsystem-Linux" -ForegroundColor Black -BackgroundColor Cyan
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+  }
 }
 
 # @description Ensure VirtualMachinePlatform feature is available
 function EnsureVirtualMachinePlatformEnabled {
-    $vmenabled = Get-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform | Select-Object -Property State
-    if ($vmenabled.State -eq "Disabled") {
-        Write-Host "Enabling VirtualMachinePlatform" -ForegroundColor Black -BackgroundColor Cyan
-        Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
-    }
+  $vmenabled = Get-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform | Select-Object -Property State
+  if ($vmenabled.State -eq "Disabled") {
+    Write-Host "Enabling VirtualMachinePlatform" -ForegroundColor Black -BackgroundColor Cyan
+    Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
+  }
 }
 
 # @description Ensures Ubuntu 22.04 is installed on the system from a .appx file
 function EnsureUbuntuAPPXInstalled {
-    if(!(Test-Path "C:\Temp\UBUNTU2204.appx")) {
-        Write-Host "Downloading Ubuntu APPX" -ForegroundColor Black -BackgroundColor Cyan
-        Start-BitsTransfer -Source "https://aka.ms/wslubuntu2204" -Destination "C:\Temp\UBUNTU2204.appx" -Description "Downloading Ubuntu 22.04 WSL image"
-    }
-    # TODO: Ensure this is the appropriate AppxPackage name
-    $Ubuntu2204APPXInstalled = Get-AppxPackage -Name CanonicalGroupLimited.Ubuntu22.04onWindows
-    if (!$Ubuntu2204APPXInstalled) {
-        Write-Host "Adding Ubuntu APPX" -ForegroundColor Black -BackgroundColor Cyan
-        Add-AppxPackage -Path "C:\Temp\UBUNTU2204.appx"
-    }
+  if(!(Test-Path "C:\Temp\UBUNTU2204.appx")) {
+    Write-Host "Downloading Ubuntu APPX" -ForegroundColor Black -BackgroundColor Cyan
+    Start-BitsTransfer -Source "https://aka.ms/wslubuntu2204" -Destination "C:\Temp\UBUNTU2204.appx" -Description "Downloading Ubuntu 22.04 WSL image"
+  }
+  # TODO: Ensure this is the appropriate AppxPackage name
+  $Ubuntu2204APPXInstalled = Get-AppxPackage -Name CanonicalGroupLimited.Ubuntu22.04onWindows
+  if (!$Ubuntu2204APPXInstalled) {
+    Write-Host "Adding Ubuntu APPX" -ForegroundColor Black -BackgroundColor Cyan
+    Add-AppxPackage -Path "C:\Temp\UBUNTU2204.appx"
+  }
 }
 
 # @description Automates the process of setting up the Ubuntu 22.04 WSL environment
 function SetupUbuntuWSL {
-    if ($ProvisionWithWSL) {
-      Write-Host "Set default WSL version to 1 (required for bridged eth0 adapter)" -ForegroundColor Black -BackgroundColor Cyan
-      wsl --set-default-version 1
-    }
-    Write-Host "Setting up Ubuntu WSL" -ForegroundColor Black -BackgroundColor Cyan
-    Start-Process "ubuntu.exe" -ArgumentList "install --root" -Wait -NoNewWindow
-    $UsernameLowercase = $env:Username.ToLower()
-    Write-Host "Adding a user" -ForegroundColor Black -BackgroundColor Cyan
-    Start-Process "ubuntu.exe" -ArgumentList "run adduser $UsernameLowercase --gecos 'First,Last,RoomNumber,WorkPhone,HomePhone' --disabled-password" -Wait -NoNewWindow
-    Write-Host "Adding user to sudo group" -ForegroundColor Black -BackgroundColor Cyan
-    Start-Process "ubuntu.exe" -ArgumentList "run usermod -aG sudo $UsernameLowercase" -Wait -NoNewWindow
-    Write-Host "Enabling passwordless sudo privileges" -ForegroundColor Black -BackgroundColor Cyan
-    Start-Process "ubuntu.exe" -ArgumentList "run echo '$UsernameLowercase ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers" -Wait -NoNewWindow
-    Write-Host "Setting default user" -ForegroundColor Black -BackgroundColor Cyan
-    Start-Process "ubuntu.exe" -ArgumentList "config --default-user $UsernameLowercase" -Wait -NoNewWindow
+  Write-Host "Set default WSL version to 1 (required for bridged eth0 adapter)" -ForegroundColor Black -BackgroundColor Cyan
+  wsl --set-default-version 1
+  Write-Host "Setting up Ubuntu WSL" -ForegroundColor Black -BackgroundColor Cyan
+  Start-Process "ubuntu.exe" -ArgumentList "install --root" -Wait -NoNewWindow
+  $UsernameLowercase = $env:Username.ToLower()
+  Write-Host "Adding a user" -ForegroundColor Black -BackgroundColor Cyan
+  Start-Process "ubuntu.exe" -ArgumentList "run adduser $UsernameLowercase --gecos 'First,Last,RoomNumber,WorkPhone,HomePhone' --disabled-password" -Wait -NoNewWindow
+  Write-Host "Adding user to sudo group" -ForegroundColor Black -BackgroundColor Cyan
+  Start-Process "ubuntu.exe" -ArgumentList "run usermod -aG sudo $UsernameLowercase" -Wait -NoNewWindow
+  Write-Host "Enabling passwordless sudo privileges" -ForegroundColor Black -BackgroundColor Cyan
+  Start-Process "ubuntu.exe" -ArgumentList "run echo '$UsernameLowercase ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers" -Wait -NoNewWindow
+  Write-Host "Setting default user" -ForegroundColor Black -BackgroundColor Cyan
+  Start-Process "ubuntu.exe" -ArgumentList "config --default-user $UsernameLowercase" -Wait -NoNewWindow
 }
 
 # @description Ensures Docker Desktop is installed (which requires a reboot)
 function EnsureDockerDesktopInstalled {
-    if (!(Test-Path "C:\Program Files\Docker\Docker\Docker Desktop.exe")) {
-      Write-Host "Installing Docker Desktop for Windows" -ForegroundColor Black -BackgroundColor Cyan
-      choco install -y docker-desktop
-      RebootAndContinue
-    }
+  if (!(Test-Path "C:\Program Files\Docker\Docker\Docker Desktop.exe")) {
+    Write-Host "Installing Docker Desktop for Windows" -ForegroundColor Black -BackgroundColor Cyan
+    choco install -y docker-desktop
+    RebootAndContinue
+  }
 }
 
 # @description Enables WinRM connectivity
 function EnableWinRM {
+  # Download and run the Ansible WinRM script
   Write-Host "Enabling WinRM.." -ForegroundColor Black -BackgroundColor Cyan
   $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
   $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
   (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
   powershell.exe -ExecutionPolicy ByPass -File $file -Verbose -EnableCredSSP -DisableBasicAuth -ForceNewSSLCert -SkipNetworkProfileCheck
+  # Generate OpenSSL configuration and encryption keys
   choco install -y -r openssl
   $UsernameLowercase = $env:Username.ToLower()
   $env:OpenSSLConfig = "C:\Temp\openssl.conf"
@@ -146,12 +146,14 @@ subjectAltName = otherName:1.3.6.1.4.1.311.20.2.3;UTF8:$UsernameLowercase@localh
   $KeyPEMPath = Join-Path "C:\Temp" key.pem
   & "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" req -x509 -nodes -days 365 -newkey rsa:2048 -out $UserPEMPath -outform PEM -keyout $KeyPEMPath -subj "/CN=$UsernameLowercase" -extensions v3_req_client 2>&1
   Remove-Item $env:OpenSSLConfig -Force
+  # Configure WinRM to use the generated configurations / credentials
   Import-Certificate -FilePath $UserPEMPath -CertStoreLocation cert:\LocalMachine\root
   $WinRMCert = Import-Certificate -FilePath $UserPEMPath -CertStoreLocation cert:\LocalMachine\TrustedPeople
   $PasswordCred = ConvertTo-SecureString -AsPlainText -Force $UserPassword
   $WinRMCreds = New-Object System.Management.Automation.PSCredential($UsernameLowercase, $PasswordCred) -ea Stop
   New-Item -Path WSMan:\localhost\ClientCertificate -Subject "$UsernameLowercase@localhost" -URI * -Issuer $WinRMCert.Thumbprint -Credential $WinRMCreds -Force
   Set-Item -Path WSMan:\localhost\Service\Auth\Certificate -Value $true
+  # Restart WinRM
   Restart-Service -Name WinRM -Force
 }
 
@@ -166,10 +168,10 @@ function RunPlaybookDocker {
 
 # @description Run the playbook with WSL
 function RunPlaybookWSL {
-    Write-Host "Running quickstart.sh in WSL environment" -ForegroundColor Black -BackgroundColor Cyan
-    Start-Process "ubuntu.exe" -ArgumentList "run curl -sSL https://gitlab.com/megabyte-labs/gas-station/-/raw/master/scripts/quickstart.sh > quickstart.sh && bash quickstart.sh" -Wait -NoNewWindow
-    Write-Host "Running quickstart continue command in WSL environment" -ForegroundColor Black -BackgroundColor Cyan
-    Start-Process "ubuntu.exe" -ArgumentList "run cd ~/Playbooks && task ansible:quickstart" -Wait -NoNewWindow
+  Write-Host "Running quickstart.sh in WSL environment" -ForegroundColor Black -BackgroundColor Cyan
+  Start-Process "ubuntu.exe" -ArgumentList "run curl -sSL https://gitlab.com/megabyte-labs/gas-station/-/raw/master/scripts/quickstart.sh > quickstart.sh && bash quickstart.sh" -Wait -NoNewWindow
+  Write-Host "Running quickstart continue command in WSL environment" -ForegroundColor Black -BackgroundColor Cyan
+  Start-Process "ubuntu.exe" -ArgumentList "run cd ~/Playbooks && task ansible:quickstart" -Wait -NoNewWindow
 }
 
 # @description Install Chocolatey
@@ -179,27 +181,29 @@ function InstallChocolatey {
 
 # @description The main logic for the script - enable Windows features, set up Ubuntu WSL, and install Docker Desktop
 # while continuing script after a restart.
-function ProvisionWindowsWSLAnsible {
-    if (!(Get-PackageProvider -Name "NuGet")) {
-      Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-    }
-    EnsureWindowsUpdated
-    EnableWinRM
-    EnsureLinuxSubsystemEnabled
-    EnsureVirtualMachinePlatformEnabled
+function ProvisionWindowsAnsible {
+  Write-Host "Ensuring Windows is updated and that pre-requisites are installed.." -ForegroundColor Black -BackgroundColor Cyan
+  if (!(Get-PackageProvider -Name "NuGet")) {
+    Write-Host "Installing NuGet since the system is missing the required version.." -ForegroundColor Black -BackgroundColor Cyan
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+  }
+  EnsureWindowsUpdated
+  InstallChocolatey
+  EnableWinRM
+  EnsureLinuxSubsystemEnabled
+  EnsureVirtualMachinePlatformEnabled
+  EnsureDockerDesktopInstalled
+  if ($ProvisionWithWSL) {
     EnsureUbuntuAPPXInstalled
     SetupUbuntuWSL
-    InstallChocolatey
-    EnsureDockerDesktopInstalled
-    if ($ProvisionWithWSL) {
-      RunPlaybookWSL
-    } else {
-      RunPlaybookDocker
-    }
-    Write-Host "All done! Make sure you change your password. It was set to 'MegabyteLabs' for automation purposes." -ForegroundColor Black -BackgroundColor Cyan
-    Read-Host "Press ENTER to exit, remove temporary files, and the start-up script"
-    Remove-Item -path "C:\Temp" -Recurse -Force
-    Remove-Item -path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Gas Station.bat" -Force
+    RunPlaybookWSL
+  } else {
+    RunPlaybookDocker
+  }
+  Write-Host "All done! Make sure you change your password. It was set to 'MegabyteLabs' for automation purposes." -ForegroundColor Black -BackgroundColor Cyan
+  Read-Host "Press ENTER to exit, remove temporary files, and the start-up script"
+  Remove-Item -path "C:\Temp" -Recurse -Force
+  Remove-Item -path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Gas Station.bat" -Force
 }
 
-ProvisionWindowsWSLAnsible
+ProvisionWindowsAnsible
