f94dc6a228c4d53c368479335d8782b70c60fc48
Brian Zalewski <brian@megabyte.space>
2022-08-22 04:31:55 +0000

    Update project-playbook/scripts/quickstart.ps1

--- project-playbook/scripts/quickstart.ps1
+++ project-playbook/scripts/quickstart.ps1
@@ -164,8 +164,8 @@ function EnableWinRM {
   Write-Host "Ensuring OpenSSL is installed" -ForegroundColor Black -BackgroundColor Cyan
   choco install -y -r openssl
   $UsernameLowercase = $env:Username.ToLower()
-  $env:OpenSSLConfig = "C:\Temp\openssl.conf"
-  Set-Content -Path $env:OpenSSLConfig -Value @"
+  $OpenSSLConfig = "C:\Temp\openssl.conf"
+  Set-Content -Path $OpenSSLConfig -Value @"
 distinguished_name = req_distinguished_name
 [req_distinguished_name]
 [v3_req_client]
@@ -176,7 +176,7 @@ subjectAltName = otherName:1.3.6.1.4.1.311.20.2.3;UTF8:$UsernameLowercase@localh
   $KeyPEMPath = Join-Path "C:\Temp" key.pem
   Write-Host "Generating PEM files with OpenSSL" -ForegroundColor Black -BackgroundColor Cyan
   & "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" req -x509 -nodes -days 365 -newkey rsa:2048 -out $UserPEMPath -outform PEM -keyout $KeyPEMPath -subj "/CN=$UsernameLowercase" -extensions v3_req_client 2>&1
-  Remove-Item $env:OpenSSLConfig -Force
+  Remove-Item $OpenSSLConfig -Force
   # Configure WinRM to use the generated configurations / credentials
   Write-Host "Importing PEM certificates" -ForegroundColor Black -BackgroundColor Cyan
   Import-Certificate -FilePath $UserPEMPath -CertStoreLocation cert:\LocalMachine\root
